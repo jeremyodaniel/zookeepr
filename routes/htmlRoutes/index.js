a282748f7628cbc2c1d3c69750091349804e9d72
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-02-13 18:59:09 -0600

    Fix multiple paths so requests will run without errors.

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,20 +6,20 @@ const router = require('express').Router();
 // The empty path- ('/') -is used to braing us to the root route of the server.
 // It creates the homepage of the server.
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepr-public/index.html'));
 });
 
 router.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepr-public/animals.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepr-public/animals.html'));
 });
 
 router.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepr-public/zookeepers.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepr-public/zookeepers.html'));
 });
 // The wildcard route should always be last or it will take precedence over any 
 // named route that comes after it.
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
+  res.sendFile(path.join(__dirname, '.././public/zookeepr-public/index.html'));
 });
 
 
